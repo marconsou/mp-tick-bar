df7925ce533ba8735c82bdf8625be01acecf7461
marconsou <marcelocontarinisouza@gmail.com>
2021-11-03 08:25:04 -0300

    Refactoring

---
+++ MPTickBar/Source/Enum/ProgressBarUI.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace MPTickBar
+{
+    public enum ProgressBarUI
+    {
+        Default,
+        [Description("Material UI Discord")]
+        MaterialUIDiscord,
+        [Description("Material UI Black")]
+        MaterialUIBlack,
+        [Description("Material UI Silver")]
+        MaterialUISilver,
+        [Description("Solid Bar")]
+        SolidBar
+    }
+}
