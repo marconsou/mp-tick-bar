ba859e8f20c98f938b31f5ee0805da3a614e2c0d
marconsou <marcelocontarinisouza@gmail.com>
2021-11-06 09:08:39 -0300

    Item spacing changes

--- MPTickBar/Source/Plugin/PluginUI.cs
+++ MPTickBar/Source/Plugin/PluginUI.cs
@@ -11,89 +11,64 @@ namespace MPTickBar
     {
         protected Configuration Configuration { get; init; }
 
-        private static float ItemSpacingVertical => 8.0f;
-
         private void SaveConfiguration<T>(T changedValue, Action<T> setter)
         {
             setter(changedValue);
             this.Configuration.Save();
         }
 
-        private static void SameLine(Vector2? sameLinePosition)
+        private static void SameLine(float spacing)
         {
-            if (sameLinePosition != null)
-                ImGui.SameLine(sameLinePosition.Value.X, sameLinePosition.Value.Y);
+            if (spacing != 0.0f)
+                ImGui.SameLine(0.0f, spacing);
         }
 
-        private static void PushItemSpacingVar()
+        protected void CheckBox(bool value, Action<bool> setter, string label, float spacing = 0.0f)
         {
-            ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, new Vector2(0.0f, PluginUI.ItemSpacingVertical));
-        }
-
-        protected void CheckBox(bool value, Action<bool> setter, string label, Vector2? sameLinePosition = null)
-        {
-            PluginUI.PushItemSpacingVar();
-
             var changedValue = value;
-            PluginUI.SameLine(sameLinePosition);
+            PluginUI.SameLine(spacing);
             if (ImGui.Checkbox(label, ref changedValue))
                 this.SaveConfiguration(changedValue, setter);
-
-            ImGui.PopStyleVar();
         }
 
-        protected void ColorEdit4(Vector4 value, Action<Vector4> setter, string label, Vector2? sameLinePosition = null)
+        protected void ColorEdit4(Vector4 value, Action<Vector4> setter, string label, float spacing = 0.0f)
         {
-            PluginUI.PushItemSpacingVar();
-
             var changedValue = value;
-            PluginUI.SameLine(sameLinePosition);
+            PluginUI.SameLine(spacing);
             if (ImGui.ColorEdit4(label, ref changedValue, ImGuiColorEditFlags.NoInputs | ImGuiColorEditFlags.AlphaBar))
                 this.SaveConfiguration(changedValue, setter);
-
-            ImGui.PopStyleVar();
         }
 
-        protected void DragFloat(float value, Action<float> setter, string label, float speed, float min, float max, string format, Vector2? sameLinePosition = null)
+        protected void DragFloat(float value, Action<float> setter, string label, float speed, float min, float max, string format, float spacing = 0.0f)
         {
-            PluginUI.PushItemSpacingVar();
-
             var changedValue = value;
-            PluginUI.SameLine(sameLinePosition);
+            PluginUI.SameLine(spacing);
             if (ImGui.DragFloat(label, ref changedValue, speed, min, max, format))
                 this.SaveConfiguration(changedValue, setter);
-
-            ImGui.PopStyleVar();
         }
 
-        protected void DragFloat(float valueLeft, float valueRight, Action<float> setterLeft, Action<float> setterRight, string label, float speed, float min, float max, string format, Vector2? sameLinePosition = null)
+        protected void DragFloat(float valueLeft, float valueRight, Action<float> setterLeft, Action<float> setterRight, string label, float speed, float min, float max, string format, float spacing = 0.0f)
         {
-            this.DragFloat(valueLeft, setterLeft, " ", speed, min, max, format, sameLinePosition);
-            this.DragFloat(valueRight, setterRight, label, speed, min, max, format, new(0.0f, PluginUI.ItemSpacingVertical));
+            this.DragFloat(valueLeft, setterLeft, " ", speed, min, max, format, spacing);
+            this.DragFloat(valueRight, setterRight, label, speed, min, max, format, ImGui.GetStyle().ItemSpacing.X * ImGuiHelpers.GlobalScale);
         }
 
-        protected void DragInt(int value, Action<int> setter, string label, int speed, int min, int max, string format, Vector2? sameLinePosition = null)
+        protected void DragInt(int value, Action<int> setter, string label, int speed, int min, int max, string format, float spacing = 0.0f)
         {
-            PluginUI.PushItemSpacingVar();
-
             var changedValue = value;
-            PluginUI.SameLine(sameLinePosition);
+            PluginUI.SameLine(spacing);
             if (ImGui.DragInt(label, ref changedValue, speed, min, max, format))
                 this.SaveConfiguration(changedValue, setter);
-
-            ImGui.PopStyleVar();
         }
 
-        protected void DragInt(int valueLeft, int valueRight, Action<int> setterLeft, Action<int> setterRight, string label, int speed, int min, int max, string format, Vector2? sameLinePosition = null)
+        protected void DragInt(int valueLeft, int valueRight, Action<int> setterLeft, Action<int> setterRight, string label, int speed, int min, int max, string format, float spacing = 0.0f)
         {
-            this.DragInt(valueLeft, setterLeft, " ", speed, min, max, format, sameLinePosition);
-            this.DragInt(valueRight, setterRight, label, speed, min, max, format, new(0.0f, PluginUI.ItemSpacingVertical));
+            this.DragInt(valueLeft, setterLeft, " ", speed, min, max, format, spacing);
+            this.DragInt(valueRight, setterRight, label, speed, min, max, format, ImGui.GetStyle().ItemSpacing.X * ImGuiHelpers.GlobalScale);
         }
 
         protected void Combo<T>(T value, Action<T> setter, string label)
         {
-            PluginUI.PushItemSpacingVar();
-
             var method = typeof(EnumExtensions).GetMethod("GetDescription", BindingFlags.Public | BindingFlags.Static, null, new[] { value.GetType() }, null);
             var options = new List<string>();
             var values = Enum.GetValues(typeof(T));
@@ -106,8 +81,6 @@ namespace MPTickBar
             var changedValue = (int)(object)value;
             if (ImGui.Combo(label, ref changedValue, options.ToArray(), options.Count))
                 this.SaveConfiguration((T)(object)changedValue, setter);
-
-            ImGui.PopStyleVar();
         }
 
         protected static void CollapsingHeader(string label, Action action)
