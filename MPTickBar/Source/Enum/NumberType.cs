eb9e901ae6e37f17f947421373cb077aa15e8319
marconsou <marcelocontarinisouza@gmail.com>
2021-11-24 06:40:56 -0300

    Added a new type of number: Remaining Time

---
+++ MPTickBar/Source/Enum/NumberType.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel;
+
+namespace MPTickBar
+{
+    public enum NumberType
+    {
+        [Description("Remaining Time")]
+        RemainingTime,
+        Percentage
+    }
+}
---
+++ MPTickBar/Source/Enum/Visibility/NumberVisibility.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace MPTickBar
+{
+    public enum NumberVisibility
+    {
+        Visible,
+        [Description("While in progress")]
+        WhileInProgress,
+        Hidden,
+    }
+}
