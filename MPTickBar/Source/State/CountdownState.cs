16c612d29dc57c6f6be5ad4ed813eee17daf240f
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2021-11-24 01:35:41 -0300

    Refactoring

--- MPTickBar/Source/State/CountdownState.cs
+++ MPTickBar/Source/State/CountdownState.cs
@@ -7,9 +7,9 @@ namespace MPTickBar
     {
         private int StartingSeconds { get; set; }
 
-        private double TimeExecuteCommand { get; set; }
+        private double ExecuteCommandTime { get; set; }
 
-        private double TimeCancelCommand { get; set; }
+        private double CancelCommandTime { get; set; }
 
         private double LastProgress { get; set; }
 
@@ -18,15 +18,15 @@ namespace MPTickBar
         private void Reset()
         {
             this.StartingSeconds = 0;
-            this.TimeExecuteCommand = 0.0;
-            this.TimeCancelCommand = 0.0;
+            this.ExecuteCommandTime = 0.0;
+            this.CancelCommandTime = 0.0;
         }
 
         public void Start(string args, int startingSeconds, float timeOffset)
         {
             this.StartingSeconds = (int.TryParse(args, out int paramValue) && Enumerable.Range(5, 26).Contains(paramValue)) ? paramValue : startingSeconds;
-            this.TimeExecuteCommand = 0.0;
-            this.TimeCancelCommand = ImGui.GetTime() + this.StartingSeconds + timeOffset + 3.5;
+            this.ExecuteCommandTime = 0.0;
+            this.CancelCommandTime = ImGui.GetTime() + this.StartingSeconds + timeOffset + 3.5;
         }
 
         public void Update(Chat chat, double progress, float timeOffset)
@@ -35,16 +35,16 @@ namespace MPTickBar
             {
                 var time = ImGui.GetTime();
 
-                if ((this.LastProgress > progress) && (this.TimeExecuteCommand == 0.0))
-                    this.TimeExecuteCommand = time + timeOffset;
+                if ((this.LastProgress > progress) && (this.ExecuteCommandTime == 0.0))
+                    this.ExecuteCommandTime = time + timeOffset;
 
-                if ((time > this.TimeExecuteCommand) && (this.TimeExecuteCommand != 0.0))
+                if ((time > this.ExecuteCommandTime) && (this.ExecuteCommandTime != 0.0))
                 {
                     chat.ExecuteCommand($"/cd {this.StartingSeconds}");
                     this.Reset();
                 }
 
-                if ((time > this.TimeCancelCommand) && (this.TimeCancelCommand != 0.0))
+                if ((time > this.CancelCommandTime) && (this.CancelCommandTime != 0.0))
                     this.Reset();
             }
             this.LastProgress = progress;
