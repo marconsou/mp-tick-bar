c1af54533b47036017b903ac61f3c9df4493bb63
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2021-11-24 20:51:20 -0300

    ProgressBarState and PlayerState rework

--- MPTickBar/Source/State/PlayerState.cs
+++ MPTickBar/Source/State/PlayerState.cs
@@ -3,7 +3,6 @@ using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.ClientState.JobGauge;
 using Dalamud.Game.ClientState.JobGauge.Types;
 using Dalamud.Game.ClientState.Objects.SubKinds;
-using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using System;
 using System.Linq;
@@ -20,37 +19,42 @@ namespace MPTickBar
 
         private PlayerCharacter Player => this.ClientState?.LocalPlayer;
 
-        public bool IsPlayingAsBlackMage => (this.ClientState != null) && (this.Player != null) && this.ClientState.IsLoggedIn && (this.Player.ClassJob?.Id == 25);
+        private Data<uint> MP { get; set; } = new();
 
-        private bool IsEffectActivated(uint statusId) => this.IsPlayingAsBlackMage && this.Player.StatusList.Any(x => x.StatusId == statusId);
+        private Data<ushort> Territory { get; set; } = new();
 
-        public bool IsLucidDreamingActivated => this.IsEffectActivated(1204);
-
-        public bool IsCircleOfPowerActivated => this.IsEffectActivated(738);
-
-        public bool IsUmbralIceActivated => this.IsPlayingAsBlackMage && (this.JobGauges != null) && (this.JobGauges.Get<BLMGauge>().UmbralIceStacks > 0);
-
-        public bool IsUmbralIceIIIActivated => this.IsPlayingAsBlackMage && (this.JobGauges != null) && (this.JobGauges.Get<BLMGauge>().UmbralIceStacks == 3);
-
-        public bool IsInCombat => this.IsPlayingAsBlackMage && (this.Condition != null) && this.Condition[ConditionFlag.InCombat];
-
-        public bool IsDead => this.IsPlayingAsBlackMage && (this.Player.CurrentHp == 0);
-
-        public ushort TerritoryType => this.IsPlayingAsBlackMage ? this.ClientState.TerritoryType : ushort.MaxValue;
-
-        public uint HP => this.IsPlayingAsBlackMage ? this.Player.CurrentHp : 0;
-
-        public uint MP => this.IsPlayingAsBlackMage ? this.Player.CurrentMp : 0;
-
-        public uint MPMax => this.IsPlayingAsBlackMage ? this.Player.MaxMp : 0;
-
-        public uint Id => this.IsPlayingAsBlackMage ? this.Player.ObjectId : uint.MaxValue;
+        private Data<bool> IsDead { get; set; } = new();
 
         public byte UmbralIceRegenStack { get; private set; }
 
         public bool LucidDreamingRegenStack { get; private set; }
 
-        public static uint LucidDreamingMPRegen => 500;
+        public bool IsPlayingAsBlackMage => (this.ClientState != null) && (this.Player != null) && this.ClientState.IsLoggedIn && (this.Player.ClassJob?.Id == 25);
+
+        public bool IsInCombat => this.IsPlayingAsBlackMage && (this.Condition != null) && this.Condition[ConditionFlag.InCombat];
+
+        public bool IsUmbralIceActivated => this.IsPlayingAsBlackMage && (this.JobGauges != null) && (this.JobGauges.Get<BLMGauge>().UmbralIceStacks > 0);
+
+        public bool IsUmbralIceIIIActivated => this.IsPlayingAsBlackMage && (this.JobGauges != null) && (this.JobGauges.Get<BLMGauge>().UmbralIceStacks == 3);
+
+        private bool IsLucidDreamingActivated => this.IsEffectActivated(1204);
+
+        private bool IsCircleOfPowerActivated => this.IsEffectActivated(738);
+
+        private bool IsEffectActivated(uint statusId) => this.IsPlayingAsBlackMage && this.Player.StatusList.Any(x => x.StatusId == statusId);
+
+        public bool CheckPlayerId(uint targetActorId) => !this.IsDead.Current && (this.Player.ObjectId == targetActorId);
+
+        private class Data<T> where T : struct
+        {
+            public T Current { get; private set; }
+
+            public T Last { get; private set; }
+
+            public void Update(T data) => this.Current = data;
+
+            public void SaveData() => this.Last = this.Current;
+        }
 
         public void ServicesUpdate(ClientState clientState, JobGauges jobGauges, Condition condition)
         {
@@ -59,12 +63,46 @@ namespace MPTickBar
             this.Condition = condition;
         }
 
-        public bool IsManafontOnCooldown()
+        public void StateUpdate()
         {
-            unsafe
+            this.MP.Update(this.IsPlayingAsBlackMage ? this.Player.CurrentMp : uint.MinValue);
+            this.Territory.Update(this.IsPlayingAsBlackMage ? this.ClientState.TerritoryType : ushort.MinValue);
+            this.IsDead.Update(this.IsPlayingAsBlackMage && (this.Player.CurrentHp == 0));
+        }
+
+        public void SaveData()
+        {
+            this.MP.SaveData();
+            this.Territory.SaveData();
+            this.IsDead.SaveData();
+        }
+
+        public void MPRegenStackUpdate()
+        {
+            var onlucidDreamingMPRegen = false;
+            if (this.IsLucidDreamingActivated)
             {
-                return this.IsPlayingAsBlackMage && ActionManager.Instance()->IsRecastTimerActive(ActionType.Spell, 158);
+                var lucidDreamingMPRegen = 500;
+                var mpRecovered = (int)(this.MP.Current - this.MP.Last);
+                var recoveringMPToFull = (this.MP.Current == this.Player.MaxMp) && (this.MP.Last > (this.Player.MaxMp - lucidDreamingMPRegen)) && (mpRecovered > 0);
+                onlucidDreamingMPRegen = (mpRecovered == lucidDreamingMPRegen || recoveringMPToFull);
             }
+            else
+                this.LucidDreamingRegenStack = false;
+
+            if (this.MP.Last > this.MP.Current)
+                this.LucidDreamingRegenStack = false;
+            else if (onlucidDreamingMPRegen)
+                this.LucidDreamingRegenStack = true;
+
+            this.UmbralIceRegenStack = (byte)((this.MP.Current + ((this.MP.Current / 6200) * 200)) / 3200);
+        }
+
+        public bool IsValidState()
+        {
+            var onZoneChange = (this.Territory.Last != this.Territory.Current);
+            var OnDeath = (!this.IsDead.Last && this.IsDead.Current);
+            return !onZoneChange && !OnDeath;
         }
 
         public float GetFastFireIIICastTime()
@@ -82,16 +120,6 @@ namespace MPTickBar
                 var spellSpeed = UIState.pInstance->PlayerState.Attributes[46];
                 return (float)Math.Floor(Math.Floor(Math.Ceiling(Math.Floor(100.0 - (this.IsCircleOfPowerActivated ? 15 : 0)) * 1) * Math.Floor((2000 - Math.Floor(130.0 * (spellSpeed - sub) / div + 1000)) * gcd35 / 1000) / 1000) * astralUmbral / 100) / 100;
             }
-        }
-
-        public void MPRegenStackUpdate(bool onMPRegenLucidDreaming, uint mpCurrent, uint mpLast)
-        {
-            this.UmbralIceRegenStack = (byte)((mpCurrent + ((mpCurrent / 6200) * 200)) / 3200);
-
-            if (mpLast > mpCurrent)
-                this.LucidDreamingRegenStack = false;
-            else if (onMPRegenLucidDreaming)
-                this.LucidDreamingRegenStack = true;
         }
     }
 }
