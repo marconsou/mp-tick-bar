d040e78708560a8b1ad9cd6a57f7e7d9965bbcde
marconsou <marcelocontarinisouza@gmail.com>
2021-12-16 15:49:21 -0300

    Level 90 and Spell Speed enabled again

--- MPTickBar/Source/Global.cs
+++ MPTickBar/Source/Global.cs
@@ -2,7 +2,7 @@ namespace MPTickBar
 {
     public static class Global
     {
-        public static byte LevelMax => 80;
+        public static byte LevelMax => 90;
 
         public static uint BlackMageId => 25;
 
@@ -15,7 +15,5 @@ namespace MPTickBar
         public static ushort MPTickOpCode => 662;
 
         public static string ChatSignature => "48 89 5C 24 ?? 57 48 83 EC 20 48 8B FA 48 8B D9 45 84 C9";
-
-        public static bool SpellSpeedDisabled = true;
     }
 }
