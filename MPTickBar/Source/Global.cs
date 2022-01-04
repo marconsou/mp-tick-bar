1ad9aeb106af12c6ec878d35323a2296a14c5ab3
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2022-01-04 11:28:26 -0300

    FFXIV patch 6.05 update

--- MPTickBar/Source/Global.cs
+++ MPTickBar/Source/Global.cs
@@ -4,7 +4,7 @@ namespace MPTickBar
     {
         public static ushort SpellSpeedIndex => 46;
 
-        public static ushort MPTickOpCode => 689;
+        public static ushort MPTickOpCode => 244;
 
         public static string ChatSignature => "48 89 5C 24 ?? 57 48 83 EC 20 48 8B FA 48 8B D9 45 84 C9";
     }
